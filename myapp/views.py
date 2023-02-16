ffb690c4976b007b58260a15494d41bb2e69c6b9
Ehsan <ehsan7007@yahoo.com>
2023-02-16 23:34:19 +0330

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,77 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-import telegram
 import os
-from telegram.ext import Updater, CommandHandler
+import openai
+import logging
+from telegram import __version__ as TG_VER
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, world!")
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
-@csrf_exempt
-def webhook(request):
-    if request.method == 'POST':
-        update = telegram.Update.de_json(request.body, telegram.Bot(token=os.environ['TOKEN']))
-        dispatcher.process_update(update)
-    return HttpResponse('ok')
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-updater = Updater(token=os.environ['TOKEN'], use_context=True)
-dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+openai.api_key =  os.environ['API_KEY']
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+
+
+
+async def AskGPT(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Asks from GPT the user message."""
+    response = openai.Completion.create(
+    model="text-curie-001",
+    prompt= update.message.text,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    await update.message.reply_text(response.choices[0].text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(os.environ['TOKEN']).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # 
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, AskGPT))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
